be656e518f42dda4ef0d9b568be96a9668df2560
Perazzojoao <perazzojoao@gmail.com>
2024-12-10 15:38:33 -0300

    Adding nestjs rest api

---
+++ nestjs-api/src/app.module.ts
@@ -0,0 +1,34 @@
+import { ConsoleLogger, Module } from '@nestjs/common';
+import { MapsModule } from './modules/maps/maps.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './resources/logger/logger.interceptor';
+import { HttpExceptionFilter } from './resources/http-exception/http-exception.filter';
+import { ConfigModule } from '@nestjs/config';
+import { RoutesModule } from './modules/routes/routes.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { RoutesDriverService } from './modules/routes/routes-driver/routes-driver.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    RoutesModule,
+    MapsModule,
+  ],
+  controllers: [],
+  providers: [
+    ConsoleLogger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    RoutesDriverService,
+  ],
+})
+export class AppModule {}
---
+++ nestjs-api/src/modules/maps/maps.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { PlacesController } from './places/places.controller';
+import { PlacesService } from './places/places.service';
+import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
+
+@Module({
+  imports: [],
+  controllers: [PlacesController, DirectionsController],
+  providers: [
+    {
+      provide: GoogleMapsClient,
+      useValue: new GoogleMapsClient({}),
+    },
+    PlacesService,
+    DirectionsService,
+  ],
+  exports: [DirectionsService],
+})
+export class MapsModule {}
