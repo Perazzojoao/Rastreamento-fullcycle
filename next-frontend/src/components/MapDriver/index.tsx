9be5dd64380a6a82bec5d72855354fae06a3b0dd
Perazzojoao <perazzojoao@gmail.com>
2024-12-12 01:44:59 -0300

    Adicionar lógica de conexão e gerenciamento de rotas no componente MapDriver

--- next-frontend/src/components/MapDriver/index.tsx
+++ next-frontend/src/components/MapDriver/index.tsx
@@ -1,11 +1,63 @@
-"use client";
+'use client'
 
-import { useRef } from "react";
-import { useMap } from "../../hooks/useMap";
+import { useEffect, useRef } from 'react'
+import { useMap } from '../../hooks/useMap'
+import { socket } from '@/utils/socket-io'
 
-export function MapDriver() {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+export type MapDriverProps = {
+  route_id: string | null
+  startLocation: { lat: number; lng: number } | null
+  endLocation: { lat: number; lng: number } | null
+}
 
-  return <div className="w-2/3 h-full" ref={mapContainerRef} />;
+export function MapDriver({ route_id, startLocation, endLocation }: MapDriverProps) {
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+  const map = useMap(mapContainerRef)
+
+  useEffect(() => {
+    if (!map || !route_id || !startLocation || !endLocation) {
+      return
+    }
+
+    if (socket.disconnected) {
+      socket.connect()
+    } else {
+      socket.offAny()
+    }
+
+    socket.on('connect', () => {
+      console.log('connected')
+      socket.emit('server:new-points', { route_id })
+    })
+
+    socket.on(`server:new-points/${route_id}:list`, (data: { route_id: string; lat: number; lng: number }) => {
+      if (!map.hasRoute(data.route_id)) {
+        map.addRouteWithIcons({
+          routeId: data.route_id,
+          startMarkerOptions: {
+            position: startLocation,
+
+          },
+          endMarkerOptions: {
+            position: endLocation,
+          },
+          carMarkerOptions: {
+            position: startLocation,
+          }
+        })
+      }
+      map.moveCar(data.route_id, { lat: data.lat, lng: data.lng })
+
+    })
+
+    return () => {
+      socket.disconnect()
+    }
+  }, [route_id, map, startLocation, endLocation])
+
+  return (
+    <div className='w-2/3 h-full'>
+      <div ref={mapContainerRef} />
+    </div>
+  )
 }
